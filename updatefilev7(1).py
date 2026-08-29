df249ac3b9c92c0eda3c4ddbbad82dbbdead4699
mgicdi69 <mhnoyon1224@gmail.com>
2026-08-29 10:21:05 +0600

    Update updatefilev7(1).py

--- updatefilev7(1).py
+++ updatefilev7(1).py
@@ -264,7 +264,7 @@ def premiumize_html(text):
 # এখানে আপনার Bot Token এবং Admin ID বসান
 # ============================================================
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8856510779:AAGebmkdR_rARymzcD6PTQLHNVQeHjnEhxA")
 
 # আপনার Telegram numeric User ID এখানে বসান
 # একাধিক Admin হলে: {123456789, 987654321}
